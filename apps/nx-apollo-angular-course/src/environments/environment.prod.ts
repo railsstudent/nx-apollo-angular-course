2a336e8be99e6900139fe00c0ff0c20ce84f4a30
Connie Leung <cleung334@gmail.com>
2021-04-05 13:34:01 +0800

    chore: GraphQLModule
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- apps/nx-apollo-angular-course/src/environments/environment.prod.ts
+++ apps/nx-apollo-angular-course/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
   production: true,
+  graphqlUrl: 'http://localhost:3333/graphql'
 };
--- apps/nx-apollo-angular-course/src/environments/environment.ts
+++ apps/nx-apollo-angular-course/src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  graphqlUrl: 'http://localhost:3333/graphql'
 };
 
 /*
