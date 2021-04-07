cabaa2a207c46be56dc832d3a5637cfc8db93361
Connie Leung <cleung334@gmail.com>
2021-04-07 23:00:45 +0800

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const { guessProductionMode } = require("@ngneat/tailwind");
+
+module.exports = {
+    prefix: '',
+    purge: {
+      enabled: guessProductionMode(),
+      content: [
+        './apps/**/*.{html,ts}',
+        './libs/**/*.{html,ts}',
+      ]
+    },
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {
+        opacity: ['disabled'],
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+};
