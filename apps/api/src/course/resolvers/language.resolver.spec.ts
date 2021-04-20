c2a20bdd90ebf7ceea4da2d57a11e1e062a1edfd
Connie Leung <cleung334@gmail.com>
2021-04-21 02:27:10 +0800

    chore: add language resolver unit tests
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- apps/api/src/course/resolvers/language.resolver.spec.ts
+++ apps/api/src/course/resolvers/language.resolver.spec.ts
@@ -3,6 +3,7 @@ import { PrismaService } from '@nx-apollo-angular-course/prisma'
 import { TranslationService, UniqueHelper } from '../services'
 import { LanguageResolver } from './language.resolver'
 import { Language } from '../entities';
+import { AddLanguageInput, UpdateLanguageInput } from '../dto';
 describe('LanguageResolver', () => {
   let resolver: LanguageResolver
   let translationService: TranslationService
@@ -31,7 +32,7 @@ describe('LanguageResolver', () => {
     expect(resolver).toBeDefined()
   })
 
-  it('getLanguages should return all courses', async () => {
+  it('getLanguages should return all languages', async () => {
     const result: Language[] = [
       {
         id: '1',
@@ -46,8 +47,54 @@ describe('LanguageResolver', () => {
         fullname: 'Spanish (Espanol)'
       }
     ]
-    jest.spyOn(translationService, 'getLanguages').mockImplementation(() => Promise.resolve([]))
+    jest.spyOn(translationService, 'getLanguages').mockImplementation(() => Promise.resolve(result))
 
     expect(await resolver.getLanguages()).toEqual(result)
+  })
+
+  it('addLanguage should create and return a language', async () => {
+    const newLanguageInput: AddLanguageInput = {
+      name: 'English',
+      nativeName: 'English'
+    }
+
+    const newLanguage: Language =
+      {
+        ...newLanguageInput,
+        id: '1',
+        fullname: 'English (English)'
+      }
+    jest.spyOn(translationService, 'addLanguage').mockImplementation(() => Promise.resolve(newLanguage))
+
+    expect(await resolver.addLanguage(newLanguageInput)).toEqual(newLanguage)
+  })
+
+
+  it('updateLanguage should update and return a language', async () => {
+    const updateLanguageInput: UpdateLanguageInput = {
+      id: '1',
+      name: 'English mod',
+      nativeName: 'English mod'
+    }
+
+    const updateLanguage: Language =
+      {
+        ...updateLanguageInput,
+        id: '1',
+        fullname: 'English (English)'
+      }
+    jest.spyOn(translationService, 'updateLanguage').mockImplementation(() => Promise.resolve(updateLanguage))
+
+    expect(await resolver.updateLanguage(updateLanguageInput)).toEqual(updateLanguage)
+  })
+
+
+  it('fullname should return the concatenation of name and nativeName', async () => {
+    const language: Language = {
+      id: '1',
+      name: 'English',
+      nativeName: 'English'
+    }
+    expect(await resolver.fullname(language)).toEqual('English (English)')
   })
 })
