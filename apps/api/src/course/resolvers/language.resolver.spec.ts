738c9a0924809ebc91cecf05b92b533a1f1cba97
Connie Leung <cleung334@gmail.com>
2021-04-21 03:19:57 +0800

    chore: add lesson resolver unit tests
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- apps/api/src/course/resolvers/language.resolver.spec.ts
+++ apps/api/src/course/resolvers/language.resolver.spec.ts
@@ -2,8 +2,9 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaService } from '@nx-apollo-angular-course/prisma'
 import { TranslationService, UniqueHelper } from '../services'
 import { LanguageResolver } from './language.resolver'
-import { Language } from '../entities';
-import { AddLanguageInput, UpdateLanguageInput } from '../dto';
+import { Language } from '../entities'
+import { AddLanguageInput, UpdateLanguageInput } from '../dto'
+
 describe('LanguageResolver', () => {
   let resolver: LanguageResolver
   let translationService: TranslationService
@@ -38,14 +39,14 @@ describe('LanguageResolver', () => {
         id: '1',
         name: 'English',
         nativeName: 'English',
-        fullname: 'English (English)'
+        fullname: 'English (English)',
       },
       {
         id: '2',
         name: 'Spanish',
         nativeName: 'Espanol',
-        fullname: 'Spanish (Espanol)'
-      }
+        fullname: 'Spanish (Espanol)',
+      },
     ]
     jest.spyOn(translationService, 'getLanguages').mockImplementation(() => Promise.resolve(result))
 
@@ -55,45 +56,41 @@ describe('LanguageResolver', () => {
   it('addLanguage should create and return a language', async () => {
     const newLanguageInput: AddLanguageInput = {
       name: 'English',
-      nativeName: 'English'
+      nativeName: 'English',
     }
 
-    const newLanguage: Language =
-      {
-        ...newLanguageInput,
-        id: '1',
-        fullname: 'English (English)'
-      }
+    const newLanguage: Language = {
+      ...newLanguageInput,
+      id: '1',
+      fullname: 'English (English)',
+    }
     jest.spyOn(translationService, 'addLanguage').mockImplementation(() => Promise.resolve(newLanguage))
 
     expect(await resolver.addLanguage(newLanguageInput)).toEqual(newLanguage)
   })
 
-
   it('updateLanguage should update and return a language', async () => {
     const updateLanguageInput: UpdateLanguageInput = {
       id: '1',
       name: 'English mod',
-      nativeName: 'English mod'
+      nativeName: 'English mod',
     }
 
-    const updateLanguage: Language =
-      {
-        ...updateLanguageInput,
-        id: '1',
-        fullname: 'English (English)'
-      }
+    const updateLanguage: Language = {
+      ...updateLanguageInput,
+      id: '1',
+      fullname: 'English (English)',
+    }
     jest.spyOn(translationService, 'updateLanguage').mockImplementation(() => Promise.resolve(updateLanguage))
 
     expect(await resolver.updateLanguage(updateLanguageInput)).toEqual(updateLanguage)
   })
 
-
   it('fullname should return the concatenation of name and nativeName', async () => {
     const language: Language = {
       id: '1',
       name: 'English',
-      nativeName: 'English'
+      nativeName: 'English',
     }
     expect(await resolver.fullname(language)).toEqual('English (English)')
   })
---
+++ apps/api/src/course/resolvers/lesson.resolver.spec.ts
@@ -0,0 +1,118 @@
+import { CursorPaginationArgs } from './../dto/cursor-pagination.dto';
+import { LessonResolver } from './lesson.resolver';
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from '@nx-apollo-angular-course/prisma'
+import { LessonService, UniqueHelper, SentenceService } from '../services'
+import { Language, Lesson, PaginatedItems } from '../entities'
+import { AddLessonInput, UpdateLessonInput } from '../dto'
+
+describe('LessonResolver', () => {
+  let resolver: LessonResolver
+  let sentenceService: SentenceService
+  let lessonService: LessonService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        LessonResolver,
+        LessonService,
+        SentenceService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: UniqueHelper,
+          useValue: {},
+        },
+      ],
+    }).compile()
+
+    resolver = module.get<LessonResolver>(LessonResolver)
+    sentenceService = module.get<SentenceService>(SentenceService)
+    lessonService = module.get<LessonService>(LessonService)
+  })
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined()
+  })
+
+  it('getLesson should return a lesson', async () => {
+    const result: Lesson =
+      {
+        id: '1',
+        name: 'Greeting',
+        course: {
+          id: '1',
+          name: 'English 101',
+          description: 'English 101 description'
+        }
+      }
+
+    const lessonId = '1'
+    jest.spyOn(lessonService, 'getLesson').mockImplementation(() => Promise.resolve(result))
+
+    expect(await resolver.getLesson(lessonId)).toEqual(result)
+  })
+
+  it('addLesson should create and return a lesson', async () => {
+    const newLessonInput: AddLessonInput = {
+      name: 'new lesson',
+      courseId: '1',
+    }
+
+    const newLesson: Lesson = {
+      id: '1',
+      name: 'new lesson',
+      course: {
+        id : '1',
+        name: 'English 101',
+        description: 'English 101 description'
+      }
+    }
+    jest.spyOn(lessonService, 'addLesson').mockImplementation(() => Promise.resolve(newLesson))
+
+    expect(await resolver.addLesson(newLessonInput)).toEqual(newLesson)
+  })
+
+  it('updateLesson should update and return a language', async () => {
+    const updateLessonInput: UpdateLessonInput = {
+      id: '1',
+      name: 'updated lesson',
+      courseId: '1'
+    }
+
+    const updateLesson: Lesson = {
+      id: '1',
+      name: 'updated lesson',
+      course: {
+        id : '1',
+        name: 'English 101',
+        description: 'English 101 description'
+      }
+    }
+    jest.spyOn(lessonService, 'updateLesson').mockImplementation(() => Promise.resolve(updateLesson))
+
+    expect(await resolver.updateLesson(updateLessonInput)).toEqual(updateLesson)
+  })
+
+  it('paginatedSentences should return sentences belonging to the lesson', async () => {
+    const items: PaginatedItems = {
+      cursor: 1000,
+      sentences: []
+    }
+
+    const lesson: Lesson = {
+      id: '1',
+      name: 'lesson 1'
+    }
+
+    const args: CursorPaginationArgs = {
+      cursor: -1,
+      limit: 2
+    }
+    jest.spyOn(sentenceService, 'getPaginatedSentences').mockImplementation(() => Promise.resolve(items))
+
+    expect(await resolver.paginatedSentences(lesson, args)).toEqual(items)
+  })
+})
