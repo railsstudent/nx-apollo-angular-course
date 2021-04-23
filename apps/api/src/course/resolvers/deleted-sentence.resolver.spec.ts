1619ea7b0838ea4bde3b50391c9d627ec6b17b24
Connie Leung <cleung334@gmail.com>
2021-04-23 08:39:13 +0800

    chore: add unit tests of delete sentence and paginate item resolvers

--- apps/api/src/course/resolvers/deleted-sentence.resolver.spec.ts
+++ apps/api/src/course/resolvers/deleted-sentence.resolver.spec.ts
@@ -1,26 +1,54 @@
+import { DeletedSentence } from './../entities/deleted-sentence.entity'
 import { Test, TestingModule } from '@nestjs/testing'
-import { SentenceService } from '../services'
+import { SentenceService, UniqueHelper } from '../services'
 import { LanguageResolver } from './language.resolver'
 import { DeletedSentenceResolver } from './deleted-sentence.resolver'
+import { PrismaService } from '@nx-apollo-angular-course/prisma'
 
 describe('DeletedSentenceResolver', () => {
   let resolver: DeletedSentenceResolver
+  let sentenceService: SentenceService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DeletedSentenceResolver,
+        SentenceService,
         {
-          provide: SentenceService,
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: UniqueHelper,
           useValue: {},
         },
       ],
     }).compile()
 
     resolver = module.get<DeletedSentenceResolver>(DeletedSentenceResolver)
+    sentenceService = module.get<SentenceService>(SentenceService)
   })
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
+  })
+
+  it('should delete sentence', async () => {
+    const result: DeletedSentence = {
+      sentence: {
+        id: '1',
+      },
+      translations: [
+        {
+          id: '1',
+          text: 'translation',
+        },
+      ],
+    }
+
+    const sentenceId = '1'
+
+    jest.spyOn(sentenceService, 'deleteSentence').mockImplementation(() => Promise.resolve(result))
+    expect(await resolver.deleteSentence(sentenceId)).toEqual(result)
   })
 })
--- apps/api/src/course/resolvers/paginated-items.resolver.spec.ts
+++ apps/api/src/course/resolvers/paginated-items.resolver.spec.ts
@@ -1,33 +1,132 @@
+import { PaginatedItems } from './../entities/paginated-items.entity'
 import { Test, TestingModule } from '@nestjs/testing'
-import { CourseService, LessonService, SentenceService } from '../services'
+import { PrismaService } from '@nx-apollo-angular-course/prisma'
+import { CursorPaginationArgs } from '../dto'
+import { CourseService, LessonService, SentenceService, UniqueHelper } from '../services'
 import { PaginatedItemsResolver } from './paginated-items.resolver'
 
 describe('PaginatedItemsResolver', () => {
   let resolver: PaginatedItemsResolver
+  let courseService: CourseService
+  let lessonService: LessonService
+  let sentenceService: SentenceService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         PaginatedItemsResolver,
+        CourseService,
+        LessonService,
+        SentenceService,
         {
-          provide: CourseService,
+          provide: PrismaService,
           useValue: {},
         },
         {
-          provide: LessonService,
-          useValue: {},
-        },
-        {
-          provide: SentenceService,
+          provide: UniqueHelper,
           useValue: {},
         },
       ],
     }).compile()
 
     resolver = module.get<PaginatedItemsResolver>(PaginatedItemsResolver)
+    courseService = module.get<CourseService>(CourseService)
+    lessonService = module.get<LessonService>(LessonService)
+    sentenceService = module.get<SentenceService>(SentenceService)
   })
 
   it('should be defined', () => {
     expect(resolver).toBeDefined()
+  })
+
+  it('should paginate courses', async () => {
+    const result: PaginatedItems = {
+      cursor: 1000,
+      courses: [
+        {
+          id: '1',
+          name: 'Course name',
+          description: 'Course description',
+        },
+      ],
+    }
+
+    const args: CursorPaginationArgs = {
+      cursor: -1,
+      limit: 2,
+    }
+
+    jest.spyOn(courseService, 'getCourses').mockImplementation(() => Promise.resolve(result))
+    expect(await resolver.courses(args)).toEqual(result)
+  })
+
+  it('should paginate lessons', async () => {
+    const result: PaginatedItems = {
+      cursor: 1000,
+      lessons: [
+        {
+          id: '1',
+          name: 'Course name',
+          course: {
+            id: '1',
+            name: 'course name',
+            description: 'course description',
+          },
+        },
+        {
+          id: '2',
+          name: 'Course name',
+          course: {
+            id: '1',
+            name: 'course name',
+            description: 'course description',
+          },
+        },
+      ],
+    }
+
+    const args: CursorPaginationArgs = {
+      cursor: -1,
+      limit: 2,
+    }
+
+    const courseId = '1'
+
+    jest.spyOn(lessonService, 'getPaginatedLessons').mockImplementation(() => Promise.resolve(result))
+    expect(await resolver.nextLessons(courseId, args)).toEqual(result)
+  })
+
+  it('should paginate sentences', async () => {
+    const result: PaginatedItems = {
+      cursor: 1000,
+      sentences: [
+        {
+          id: '1',
+          text: 'Sentence 1',
+          lesson: {
+            id: '1',
+            name: 'lesson name',
+          },
+        },
+        {
+          id: '2',
+          text: 'Sentence 2',
+          lesson: {
+            id: '1',
+            name: 'lesson name',
+          },
+        },
+      ],
+    }
+
+    const args: CursorPaginationArgs = {
+      cursor: -1,
+      limit: 2,
+    }
+
+    const lessonId = '1'
+
+    jest.spyOn(sentenceService, 'getPaginatedSentences').mockImplementation(() => Promise.resolve(result))
+    expect(await resolver.nextSentences(lessonId, args)).toEqual(result)
   })
 })
