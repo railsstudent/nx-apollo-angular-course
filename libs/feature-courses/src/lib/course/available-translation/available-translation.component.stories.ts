775209f2c22ffb127dd68f99f5fe561ec1183b62
Connie Leung <cleung334@gmail.com>
2021-07-10 13:31:09 +0800

    chore: storybook for available translation component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

---
+++ libs/feature-courses/src/lib/course/available-translation/available-translation.component.stories.ts
@@ -0,0 +1,39 @@
+import { action } from '@storybook/addon-actions';
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { AvailableTranslationComponent } from './available-translation.component';
+
+export default {
+  title: 'AvailableTranslationComponent',
+  component: AvailableTranslationComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [],
+    })
+  ],
+} as Meta<AvailableTranslationComponent>;
+
+export const actionData = {
+  deleteTranslation: action('deleteTranslation'),
+  showTranslation: action('showTranslation'),
+  closeTranslation: action('closeTranslation'),
+}
+
+const Template: Story<AvailableTranslationComponent> = (args: AvailableTranslationComponent) => ({
+  component: AvailableTranslationComponent,
+  props: {
+    ...args,
+    ...actionData,
+  }
+});
+
+const availableTranslations = [
+  { id: '101', name: 'Chinese', text: '早安' },
+  { id: '102', name: 'English', text: 'Good Morning' },
+  { id: '103', name: 'Portuguese', text: 'Bom dia' },
+]
+
+export const Primary = Template.bind({});
+Primary.args = {
+    availableTranslations,
+    selectedTranslation:  availableTranslations[0],
+}
