a474889116dd3edc5ca9a29b6f56aea5634d8158
Connie Leung <cleung334@gmail.com>
2021-08-26 08:43:13 +0800

    fix: npm vulnerabilities
    Signed-off-by: Connie Leung <cleung334@gmail.com>
    
    fix: npm vulnerabilities
    
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,3 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/nx-apollo-angular-course',
-    '<rootDir>/apps/api',
-    '<rootDir>/libs/data-access',
-    '<rootDir>/libs/feature-courses',
-    '<rootDir>/libs/ui-courses',
-    '<rootDir>/apps/api-e2e',
-  ],
-}
+const { getJestProjects } = require('@nrwl/jest')
+
+module.exports = { projects: getJestProjects() }
